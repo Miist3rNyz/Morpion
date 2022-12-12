37e2a8143eecf13eccba9984fa7d7c67d485d99a
Kevin <kevinkesteloot76@gmail.com>
2022-12-12 22:49:59 +0100

    version finale

---
+++ Morpion/Jeu.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morpion
+{
+    public class Jeu
+    {
+        private Cellule[,] _grille; // Tableau à deux dimensions.
+
+        public Jeu()
+        {
+            _grille = new Cellule[3, 3]; // Par défaut, le tableau contient des zéros => Cellule.Rien.
+        }
+
+        public bool Coche(bool joueur, int i, int j)
+        {
+            if (_grille[i, j] == Cellule.Rien)
+            {
+                if (joueur)
+                {
+                    _grille[i, j] = Cellule.Croix;
+                }
+                else
+                {
+                    _grille[i, j] = Cellule.Rond;
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Gagnant(out bool joueur)
+        {
+            Cellule c;
+
+            for (int k = 0; k < 3; k++)
+            {
+                // Parcours des lignes.
+                c = _grille[k, 0];
+                if (c != Cellule.Rien && c == _grille[k, 1] && c == _grille[k, 2])
+                {
+                    joueur = (c == Cellule.Croix);
+                    return true;
+                }
+
+                // Parcours des colonnes.
+                c = _grille[0, k];
+                if (c != Cellule.Rien && c == _grille[1, k] && c == _grille[2, k])
+                {
+                    joueur = (c == Cellule.Croix);
+                    return true;
+                }
+            }
+
+            // Parcours des diagonales.
+            c = _grille[0, 0];
+            if (c != Cellule.Rien && c == _grille[1, 1] && c == _grille[2, 2])
+            {
+                joueur = (c == Cellule.Croix);
+                return true;
+            }
+
+            c = _grille[0, 2];
+            if (c != Cellule.Rien && c == _grille[1, 1] && c == _grille[2, 0])
+            {
+                joueur = (c == Cellule.Croix);
+                return true;
+            }
+
+            joueur = false; // Valeur arbitraire.
+            return false;
+        }
+    }
+
+    public enum Cellule
+    {
+        Rien,
+        Croix,
+        Rond
+    }
+}
---
+++ Morpion/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Morpion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private  Jeu _jeu;
+        private bool _joueur;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _jeu    = new Jeu();
+            _joueur = true; // Les X commencent.
+        }
+
+        private void CelluleButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button bt = (Button)sender;
+
+            int i = Grid.GetRow(bt);    // XAML : Grid.Row="..."
+            int j = Grid.GetColumn(bt); //        Grid.Column="..."
+
+            if (_jeu.Coche(_joueur, i, j))
+            {
+                if (_joueur)
+                {
+                    bt.Content = "X";
+                }
+                else
+                {
+                    bt.Content = "O";
+                }
+
+                if (_jeu.Gagnant(out bool gagnant))
+                {
+                    if (gagnant)
+                        MessageBox.Show("Les X ont gagné");
+                    else
+                        MessageBox.Show("Les O ont gagné");
+                }
+
+                _joueur = !_joueur;
+            }
+        }
+
+        private void RecommencerButton_Click(object sender, RoutedEventArgs e)
+        {
+            _jeu    = new Jeu();
+            _joueur = true; // Les X commencent.
+
+            // /!\ Dans le cas général, une Grid peut contenir n'importe quel contrôle.
+            //     Dans ce cas particulier, il n'y a que des Button.
+            foreach (Button bt in _celluleGrid.Children)
+            {
+                bt.Content = null;
+            }
+        }
+    }
+}
